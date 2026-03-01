5603954bc76a972e9f70b0b2266394e415f4d291
Cargoland Project <cargolandproject@gmail.com>
2026-03-01 06:04:36 +0100

    cors allowed

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,11 @@ async function bootstrap() {
   app.useGlobalFilters(new GlobalExceptionFilter());
   app.useGlobalInterceptors(new ResponseEnvelopeInterceptor());
 
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
