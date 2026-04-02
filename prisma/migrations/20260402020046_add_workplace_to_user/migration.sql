290f0a24d534f8937693b47cd3b9ec3b6bb47e74
Cargoland Project <cargolandproject@gmail.com>
2026-04-02 10:01:21 +0100

    updates

---
+++ prisma/migrations/20260402020046_add_workplace_to_user/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "workplaceArea" TEXT,
+ADD COLUMN     "workplaceCity" TEXT,
+ADD COLUMN     "workplaceName" TEXT,
+ADD COLUMN     "workplaceState" TEXT;
